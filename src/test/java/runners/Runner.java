b588a6a9f7bdb849aba36bbbe91c47f051898212
Nurullah Taşkan <nttaskan@gmail.com>
2023-02-27 17:11:28 +0300

    27/02/2023

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,9 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",//raporlarin daha okunakli olmasi icin
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
+
         features = "./src/test/resources/features",//features folder path
-        glue = "stepdefinitions",//stepdefinitions path
-        tags = "@scenario_outline_1",
+        glue = "stepdefinitions",   //stepdefinitions path
+        tags = "@personel_olusturma",
         dryRun = false
 
 
